9e970845008b27334208b389c23b81dcba9c98d6
phill765 <league@WTS13.attlocal.net>
2016-08-10 11:53:36 -0700

    asddf

--- src/day3/SpeakAndSpell.java
+++ src/day3/SpeakAndSpell.java
@@ -14,17 +14,30 @@ import javax.swing.JOptionPane;
 public class SpeakAndSpell {
 
 	public static void main(String[] args) {
-		// 1. Use the speak method to say the word. "e.g. spell mandlebrot"
-
+		// 1. Use the speak method to say the word. "e.g. spell Mandelbrot"
+		
+		speak("spell Mandelbrot");
+		String ans=JOptionPane.showInputDialog("Spell it here");
 		// 2. Catch the user's answer in a String
 
 		// 3. If the user spelled the word correctly, speak "correct"
+		if(ans.equalsIgnoreCase("mandlebrot")){
+		JOptionPane.showMessageDialog(null, "correct");	
+			
+			
+		}
+		else{
+		JOptionPane.showMessageDialog(null, "wrong");
+		}
+		
+		}
 
 		// 4. Otherwise say "wrong"
+	
 
 		// 5. repeat the process for other words
 		
-	}
+	
 
 	static void speak(String words) {
 		try {
